f48540580e1779fe0bf31d1a434d8f056be244fa
Shree Sampath <Shree Sampath>
2016-01-30 20:07:01 -0500

    <idk>

--- www/js/app.js
+++ www/js/app.js
@@ -76,7 +76,10 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         templateUrl: 'templates/tab-menu.html',
         controller: 'MenuCtrl'
   
-  ;
+      }
+	}
+  })
+  
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
